8540558d6de0abc262531b53905aa91181c41cc0
Martijn Gastkemper <martijngastkemper@gmail.com>
2021-03-14 12:27:26 +0100

    Put storage functionality in seperate class

--- main.py
+++ main.py
@@ -1,27 +1,18 @@
 import asyncio
 import timed_input
-import os
-import pickle
+import storage
 
 from bleak import BleakClient
 
 clientCalibrationVersion = b'\x00\x00\x00\x01';
 
-FACET_DICTIONARY_FILE = "facet.dictionary"
+storage = storage.Repository()
+storage.load()
 
 CALIBRATION_UUID = "F1196F56-71A4-11E6-BDF4-0800200C9A66"
 FACET_UUID = "F1196F52-71A4-11E6-BDF4-0800200C9A66"
 PASSWORD_UUID = "F1196F57-71A4-11E6-BDF4-0800200C9A66"
 
-try:
-    with open(FACET_DICTIONARY_FILE, 'r+b') as handler:
-        try:
-            facetDictionary = pickle.load(handler)
-        except EOFError:
-            facetDictionary = {}
-except FileNotFoundError:
-    open(FACET_DICTIONARY_FILE, 'w')
-    facetDictionary = {}
 
 async def run(address):
     async with BleakClient(address) as client:
@@ -31,24 +22,25 @@ async def run(address):
         """ Set calibration version after Timeflip reset """
         server_calibration_version = await client.read_gatt_char(CALIBRATION_UUID)
         if server_calibration_version != clientCalibrationVersion:
-            os.remove(FACET_DICTIONARY_FILE)
+            storage.reset()
             await client.write_gatt_char(CALIBRATION_UUID, clientCalibrationVersion)
 
         """ Handle uncalibrated facet """
         async def calibrate_facet(facetId):
             icon_name = await timed_input.timed_input("Which icon do you see? ", 5)
-            facetDictionary[facetId] = icon_name
+            if icon_name is None:
+                return
+            storage.calibrate_facet(facetId, icon_name)
             loop.create_task(print_icon(icon_name))
-            with open(FACET_DICTIONARY_FILE, 'w+b') as handler:
-                pickle.dump(facetDictionary, handler)
 
         """ Handle facet change """
+
         def facet_handler(sender, data):
             if data == b'':
                 loop.create_task(handle_error("Facet data is empty probable wrong password."))
                 return
 
-            icon = facetDictionary.get(data[0])
+            icon = storage.get_icon(data[0])
             if icon is None:
                 loop.create_task(calibrate_facet(data[0]))
             else:
@@ -67,7 +59,7 @@ async def run(address):
             await asyncio.sleep(1)
 
 if __name__ == "__main__":
-    address = ("2B160B66-3467-472D-B8D1-E2BD976DBFFE")
+    address = "2B160B66-3467-472D-B8D1-E2BD976DBFFE"
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(run(address))
---
+++ storage.py
@@ -0,0 +1,36 @@
+import pickle
+
+FACET_DICTIONARY_FILE = "facet.dictionary"
+
+
+class Repository:
+
+    def __init__(self):
+        self.facet_dictionary = {}
+
+    def calibrate_facet(self, facet, icon):
+        self.facet_dictionary[facet] = icon
+        self.persist()
+
+    def get_icon(self, facet):
+        print(self.facet_dictionary)
+        return self.facet_dictionary.get(facet)
+
+    def load(self):
+        try:
+            with open(FACET_DICTIONARY_FILE, 'r+b') as handler:
+                try:
+                    self.facet_dictionary = pickle.load(handler)
+                except EOFError:
+                    self.facet_dictionary = {}
+        except FileNotFoundError:
+            self.facet_dictionary = {}
+            self.persist()
+
+    def persist(self):
+        with open(FACET_DICTIONARY_FILE, 'w+b') as handler:
+            pickle.dump(self.facet_dictionary, handler)
+
+    def reset(self):
+        self.facet_dictionary = {}
+        self.persist()
