173b3ab7c41ae9fd6e1b29f5ae5ad25124afece4
Martijn Gastkemper <martijngastkemper@gmail.com>
2021-03-15 08:29:59 +0100

    Select time_entry and start when a facet is up

--- main.py
+++ main.py
@@ -32,11 +32,12 @@ async def run(address: str):
             await client.write_gatt_char(CALIBRATION_UUID, clientCalibrationVersion)
 
         async def calibrate_facet(facet: int):
-            icon_name = await timed_input.timed_input("Which icon do you see? ", 5)
-            if icon_name is None:
+            time_entry_id = await timed_input.timed_input("Which time entry do you want to start? ", 5)
+            if time_entry_id is None:
                 return
-            storage.calibrate_facet(facet, icon_name)
-            loop.create_task(print_icon(icon_name))
+            storage.calibrate_facet(facet, time_entry_id)
+            loop.create_task(print_icon(time_entry_id))
+            loop.create_task(start_time_entry(time_entry_id))
 
         def facet_handler(sender, data):
             if data == b'':
@@ -48,6 +49,7 @@ async def run(address: str):
                 loop.create_task(calibrate_facet(data[0]))
             else:
                 loop.create_task(print_icon(icon))
+                loop.create_task(start_time_entry(icon))
 
         async def handle_error(message: str):
             print("An error occurred, disconnecting: {0}".format(message))
@@ -55,6 +57,9 @@ async def run(address: str):
 
         async def print_icon(icon: str):
             print("Icon {0} is now up".format(icon))
+
+        async def start_time_entry(time_entry_id):
+            print(productive.start_time_entry(time_entry_id))
 
         await client.start_notify(FACET_UUID, facet_handler)
 
@@ -64,6 +69,8 @@ async def run(address: str):
 
 if __name__ == "__main__":
     address = "2B160B66-3467-472D-B8D1-E2BD976DBFFE"
+
+    print(productive.time_entries())
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(run(address))
